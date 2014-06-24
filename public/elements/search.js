70c2c33d09effe6604153f0d19d9b22e5c364985
Zaturrby <rjkorteschiel@gmail.com>
2014-06-24 09:19:33 +0200

    add first iteration of force diagram and other small changes

--- public/elements/search.js
+++ public/elements/search.js
@@ -1,16 +1,30 @@
 // Search element Javascript
 
-Polymer('search-element', { teller: 0, search: "", display: "", tagsList: "", sizedTagsList: "",
+Polymer('search-element', { teller: 0, search: "", displaySearch: "", tagsList: "",
    ready: function() {
 
         console.log(opendata);
+        var openDataTags = [];
 
         for (var i = 0; i < opendata.length; i++) {
              opendata[i].tagscomma = opendata[i].tagscomma.split(", ");
         };
 
-        this.tagsCounter();
-        this.cloudGenerate();
+
+        for (var i = 0; i < opendata.length; i++) {
+             for (var s = 0; s < opendata[i].tagscomma.length; s++) {
+                openDataTags.push(opendata[i].tagscomma[s])
+             };
+        };     
+
+        console.log(openDataTags); 
+
+        this.tagsList = this.tagsCounter(openDataTags);
+
+        //this.cloudGenerate(this.tagsList);
+        this.generateForceGraph();
+
+
 
    },
 
@@ -18,86 +32,91 @@ Polymer('search-element', { teller: 0, search: "", display: "", tagsList: "", si
    searchChanged: function() {
         this.displaySearch = [];
         this.teller = 0;     
+        this.tagsList =[];
 
         for (var i = 0; i < opendata.length; i++) {
              for (var s = 0; s < opendata[i].tagscomma.length; s++) {
                   if (opendata[i].tagscomma[s] === this.search) {
-                       this.teller++;
-                       var temp  = { hit: opendata[i].title };
-                       this.displaySearch.push(temp); 
-                  };
+                      this.teller++;
+                      var temp  = { hit: opendata[i].title, link: opendata[i].url};
+                      this.displaySearch.push(temp);
+                       
+                      for (var r = 0; r < opendata[i].tagscomma.length; r++) {
+                            this.tagsList.push(opendata[i].tagscomma[r]);
+                      }
+                  }
 
              };
-        };     
+        };         
 
-
+        console.log(this.tagsList)
+        this.tagsList = this.tagsCounter(this.tagsList);
+        if (this.tagsList.length != 0) {
+        this.cloudGenerate();
+        }
    },
 
-   tagsCounter: function() {
+   tagsCounter: function(tags) {
 
         // count the tags!
 
-        this.tagsList = [];
+        var proccesedTags = [];
 
-        for (var i = 0; i < opendata.length; i++) {
-             for (var s = 0; s < opendata[i].tagscomma.length; s++) {
+        for (var i = 0; i < tags.length; i++) {
                   
                   var tagsBolean = false;
 
-                  if (this.tagsList.length == 0) {
-                       var temp  = { text: opendata[i].tagscomma[s], size: 1 };
-                       this.tagsList.push(temp);
+                  if (proccesedTags.length == 0) {
+                       var temp  = { text: tags[i], size: 1 };
+                       proccesedTags.push(temp);
 
                   } else {
 
-                       for (var r = 0; r < this.tagsList.length; r++) {
+                       for (var r = 0; r < proccesedTags.length; r++) {
 
-                            if (opendata[i].tagscomma[s] == this.tagsList[r].text) {
+                            if (tags[i] == proccesedTags[r].text) {
                                  tagsBolean = true;
-                                 this.tagsList[r].size++;
-                                 //break;
+                                 proccesedTags[r].size++;
                             }
                        };     
 
                        if (tagsBolean == false) {
-                            var temp  = { text: opendata[i].tagscomma[s], size: 1 };
-                            this.tagsList.push(temp);
-                            //break;
-                       }  
-                            
-                  }; 
+                            var temp  = { text: tags[i], size: 1 };
+                            proccesedTags.push(temp);
+                       }   
                   
              };
         };
 
         // putting the biggest tag on top of the array
 
-        var tagsListOrder = [];
+        var proccesedTagsOrder = [];
 
         // The loop changes the length of the array. But I want it to completly empty out the array. This determines how often the loop will run, before the loop starts. 
 
-        var tagsListLength = this.tagsList.length;
+        var proccesedTagsLength = proccesedTags.length;
 
-        for (var i = 0; i < tagsListLength; i++) {
+        for (var i = 0; i < proccesedTagsLength; i++) {
              
              var highest = 0;
 
-             for (var r = 0; r < this.tagsList.length; r++) { 
-                  if (this.tagsList[r].size > highest) {
-                       highest = this.tagsList[r].size;
-                       highesthit = this.tagsList[r];
+             for (var r = 0; r < proccesedTags.length; r++) { 
+                  if (proccesedTags[r].size > highest) {
+                       highest = proccesedTags[r].size;
+                       highesthit = proccesedTags[r];
                        highestr = r;
                   }
              };
-             tagsListOrder.push(highesthit);
-             this.tagsList.splice(highestr,1);
+             proccesedTagsOrder.push(highesthit);
+             proccesedTags.splice(highestr,1);
         };
 
-        this.tagsList = tagsListOrder;
+        proccesedTags = proccesedTagsOrder;
 
+        return proccesedTags;
    },
    
-   cloudGenerate: function() {
+   cloudGenerate: function(arrayToBeClouded) {
 
         // Give the tags nice value's to be inputted to the D3 cloud generator, and run it.
 
@@ -117,19 +136,19 @@ Polymer('search-element', { teller: 0, search: "", display: "", tagsList: "", si
         };
 
 
-        this.sizedTagsList = resizeArray(this.tagsList);
+        var sizedTagsList = resizeArray(arrayToBeClouded);
 
-        // generate the wordcloud
+        
+        // Self is important because 'this' means something else to the D3 object. D3 is quite full of itself.
+        selfWordCloud = this.shadowRoot.getElementById('wordCloudLive');
+
+        // generate the wordcloud 
 
         var fill = d3.scale.category20();
 
-        // Self is important because 'this' means something else to the D3 object. D3 is quite full of itself. 
-
-        selfWordCloud = this.shadowRoot.getElementById('wordCloudLive');
-
         d3.layout.cloud()
              .size([900, 600])
-             .words(this.sizedTagsList)
+             .words(sizedTagsList)
              .padding(1)
              .rotate(0)
              .font("Impact")
@@ -156,8 +175,115 @@ Polymer('search-element', { teller: 0, search: "", display: "", tagsList: "", si
                   return "translate(" + [d.x, d.y] + ")rotate(" + d.rotate + ")";
                 })
              .text(function(d) { return d.text; });
-          }
+          } 
 
 
-   }                    
+   },
+
+   queryInput: function(query) {
+    console.log(query);
+      this.search = query;
+   },
+
+   home: function () {
+    this.queryInput('wonen')
+   },
+   globe: function () {
+    this.queryInput('aardrijkskunde')
+   },
+   personIcon: function () {
+    this.queryInput('ocw')
+   },
+   nature: function () {
+    this.queryInput('environment')
+   },
+   money: function () {
+    this.queryInput('begroting')
+   },
+   road: function () {
+    this.queryInput('transportation')
+   },
+
+   generateForceGraph: function () {
+        // Generating the neccecairy linking data. 
+
+        for (var i = 0; i < opendata.length; i++) {
+             for (var s = 0; s < opendata[i].tagscomma.length; s++) {
+                openDataTags.push(opendata[i].tagscomma[s])
+
+             };
+        }; 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        var width = 960,
+            height = 800;
+
+        var color = d3.scale.category20();
+
+        var force = d3.layout.force()
+            .charge(-120)
+            .linkDistance(80)
+            .size([width, height]);
+
+
+        // Destroying D3's sense of identity    
+        selfForceGraph = this.shadowRoot.getElementById('forceGraph');      
+        console.log(d3.select(selfForceGraph));
+
+
+        var svg = d3.select(selfForceGraph).append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+        d3.json("elements/miserables.json", function(error, graph) {
+          force
+              .nodes(graph.nodes)
+              .links(graph.links)
+              .start();
+
+          var link = svg.selectAll(".link")
+              .data(graph.links)
+            .enter().append("line")
+              .attr("class", "link")
+              .style("stroke-width", function(d) { return Math.sqrt(d.value); });
+
+          var node = svg.selectAll(".node")
+              .data(graph.nodes)
+            .enter().append("circle")
+              .attr("class", "node")
+              .attr("r", 10 )
+              .style("fill", function(d) { return color(d.group); })
+              .call(force.drag);
+
+          node.append("title")
+              .text(function(d) { return d.name; });
+
+          force.on("tick", function() {
+            link.attr("x1", function(d) { return d.source.x; })
+                .attr("y1", function(d) { return d.source.y; })
+                .attr("x2", function(d) { return d.target.x; })
+                .attr("y2", function(d) { return d.target.y; });
+
+          node.attr("cx", function(d) { return d.x; })
+            .attr("cy", function(d) { return d.y; });
+  });
+});
+   }
+
+
 });
